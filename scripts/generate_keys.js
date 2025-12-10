fe26d1edc106e8c47ee8063ba314ed259ff0697e
harshaithireddy <22a91a12a1@aec.edu.in>
2025-12-10 18:00:25 +0530

    final submission

---
+++ scripts/generate_keys.js
@@ -0,0 +1,24 @@
+import fs from 'node:fs';
+import crypto from 'node:crypto';
+
+function generateRsaKeypair() {
+  const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
+    modulusLength: 4096,
+    publicExponent: 0x10001,
+    publicKeyEncoding: {
+      type: 'spki',
+      format: 'pem'
+    },
+    privateKeyEncoding: {
+      type: 'pkcs8',
+      format: 'pem'
+    }
+  });
+
+  fs.writeFileSync('student_private.pem', privateKey, { mode: 0o600 });
+  fs.writeFileSync('student_public.pem', publicKey, { mode: 0o644 });
+
+  console.log('Generated student_private.pem and student_public.pem');
+}
+
+generateRsaKeypair();
